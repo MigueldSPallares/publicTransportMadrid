4b97db91b16339e9d22a48602f9eb40e7e5bc13b
MigueldSPallares <migueldspa2@gmail.com>
2025-04-11 12:24:48 +0200

    Update Stop.py

--- Stop.py
+++ Stop.py
@@ -1,30 +1,45 @@
 from owlready2 import *;
 import pandas as pd;
+import os;
 
 
-ontology = get_ontology("D:/publicTransportMadrid.rdf").load()
+if os.path.exists("D:/publicTransportMadrid_full.rdf"):
+    ontology = get_ontology("D:/publicTransportMadrid_full.rdf").load()
+else:
+    ontology = get_ontology("D:/publicTransportMadrid.rdf").load()
 
 data = pd.read_csv("C:/Users/migue/Downloads/google_transit_M4/stops.txt")
 
-print(list(ontology.Stop.subclasses()))
 for index, row in data.iterrows():
-    if(row['location_type']== 1):
-        newStop = ontology.Station(row['stop_id'])
+        print(row['stop_id'])
+        if(row['location_type']== 1):
+            newStop = ontology.Station(row['stop_id'])
+            newStop.hasName.append(row['stop_name'])
+            newStop.hasDescription.append(row['stop_desc'])
+            newStop.latitude.append(row['stop_lat'])
+            newStop.longitude.append(row['stop_lon'])
 
 for index, row in data.iterrows():
     if(row['location_type']== 0):
         newStop = ontology.NormalStop(row['stop_id'])
-        newStop.hasName.append(row['stop_name'])
-        newStop.hasDescription.append(row['stop_desc'])
-        newStop.latitude.append(row['stop_lat'])
-        newStop.longitude.append(row['stop_lon'])
     elif(row['location_type'] == 2):
         newStop = ontology.Entrance_Exit(row['stop_id'])
     elif(row['location_type'] == 3):
         newStop = ontology.GenericNode(row['stop_id'])
     elif(row['location_type'] == 4):
         newStop = ontology.BoardingArea(row['stop_id'])
+    
+    newStop.hasName.append(row['stop_name'])
+    if((pd.isnull(row['stop_desc']))==False):
+        newStop.hasDescription.append(row['stop_desc'])
+    newStop.latitude.append(row['stop_lat'])
+    newStop.longitude.append(row['stop_lon'])
+    if(row['wheelchair_boarding'] == 0):
+        newStop.isWheelchairAccesible.append(ontology.wheelchairAccesibilityUnknown)
+    if(row['wheelchair_boarding'] == 1):
+        newStop.isWheelchairAccesible.append(ontology.IsWheelchairAccesible)
+    if(row['wheelchair_boarding'] == 2):
+        newStop.isWheelchairAccesible.append(ontology.IsNotWheelchairAccesible)
 
 
-
-print(list(ontology.data_properties())) 
+ontology.save(file = "D:/publicTransportMadrid_full.rdf")
